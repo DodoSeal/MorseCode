96bf5e4402fdf214eacf83842aa2ab8d283f27d5
DodoSeal <maxhmcd08@gmail.com>
2026-07-12 13:38:20 -0600

    Remove debug HTML page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
 import { MorseUtil } from "./morse/util.js";
-
-const translationString = "Hello, World!";
-
-const originalLabel = document.getElementById("original")!;
-originalLabel.innerText = translationString;
-
-const morseLabel = document.getElementById("morse")!;
-morseLabel.innerText = MorseUtil.TranslateStringIntoCode(translationString)!;
